0ce81157ccce288003b1fbb2442f45fb21939c69
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-03-13 10:44:01 -0400

    :sparkles: [feat] user-service

---
+++ back-end/src/services/user.service.js
@@ -0,0 +1,30 @@
+const { user } = require('../database/models');
+
+const createUser = async (newUser) => {
+    const createdUser = await user.create(newUser);
+    return createdUser;
+};
+
+const editUser = async (id, updatedUser) => {
+    const [updated] = await user.update(updatedUser, {
+        where: { id },
+    });
+    return updated;
+};
+
+const getAllUsers = async () => {
+    const users = await user.findAll();
+    return users;
+};
+
+const getUserById = async (id) => {
+    const u = await user.findByPk(id);
+    return u;
+};
+
+module.exports = {
+    createUser,
+    editUser,
+    getAllUsers,
+    getUserById,
+};
