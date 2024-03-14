19e3e82933dd2fba3f4ef975510c92aa820f980a
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-03-14 09:56:21 -0400

    :lipstick: [style] new-client-button

--- front-end/src/pages/Home/index.jsx
+++ front-end/src/pages/Home/index.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 export default function Home() {
   return (
     <div className="container">
       <div className="d-flex flex-direction-row justify-content-between">
         <div>
-          <h4>Listagem de usuários</h4>
-          Escolha um cliente para visializar os detalhes
+          <h4
+            className="text-secondary"
+          >
+            Listagem de usuários
+          </h4>
+          <span className="text-secondary fs-5">
+            Escolha um cliente para visializar os detalhes
+          </span>
         </div>
         <div>
-          Novo cliente
+          <Button className="button-orange">
+            <span className="text-white fs-5">
+              Novo Cliente
+            </span>
+          </Button>
         </div>
       </div>
     </div>
