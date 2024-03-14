28ffff142a852d3f7782e036f516c06ac9d8712c
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-03-14 09:29:01 -0400

    :lipstick: [style] navbar

--- front-end/src/pages/Home/index.jsx
+++ front-end/src/pages/Home/index.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="container">
+      <div className="d-flex flex-direction-row justify-content-between">
+        <div>
+          <h4>Listagem de usuários</h4>
+          Escolha um cliente para visializar os detalhes
+        </div>
+        <div>
+          Novo cliente
+        </div>
+      </div>
     </div>
   );
 }
--- front-end/src/pages/LayoutComponents/Navbar.jsx
+++ front-end/src/pages/LayoutComponents/Navbar.jsx
@@ -3,8 +3,18 @@ import logoUol from '../../assets/logo/uol-logo.svg';
 
 export default function NavbarComponent() {
   return (
-    <div className="navbar d-flex justify-content-center">
-      <img src={ logoUol } alt="UOL Logo" />
+    <div>
+      <div className="navbar d-flex justify-content-center">
+        <img src={ logoUol } alt="UOL Logo" />
+      </div>
+      <div
+        className="container
+       d-flex justify-content-start align-items-baseline mt-5 pt-5 mb-3"
+      >
+        <i className="fs-1 bi bi-person me-3 " />
+        <h1>Painel de clientes</h1>
+      </div>
+      <div className="container border border-secundery mb-5" />
     </div>
   );
 }
